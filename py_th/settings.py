89446d45402832894043bba98a3f118836311ac4
Aflah2635 <85181024+Aflah2635@users.noreply.github.com>
2025-05-18 18:23:23 +0530

    Delete settings.py

--- py_th/settings.py
+++
@@ -1,9 +0,0 @@
-# Add these settings for CSRF
-CSRF_TRUSTED_ORIGINS = [
-    'https://th-af.up.railway.app'
-]
-
-# If you're using CORS, also add:
-CORS_ALLOWED_ORIGINS = [
-    'https://th-af.up.railway.app'
-]
